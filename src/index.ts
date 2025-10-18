53a118dcafd10e0ca3476bba36b90d2ad46f4f7f
unknown <yonatanvigiliolavado09@gmail.com>
2025-10-18 01:21:05 -0500

    feat: add vercel

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import { fileURLToPath } from "node:url";
 import dotenv from "dotenv";
 import express from "express";
-import path, { dirname } from "node:path";
+import path from "node:path";
 dotenv.config({ path: ".env" });
 const port = process.env.PORT;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -14,7 +11,7 @@ app.use(express.static(path.resolve(__dirname, "../public")));
 app.use((_, res) => {
     res.redirect("https://github.com/VigilioYonatan?tab=repositories");
 });
-app.get("/hello-world", () => {
+app.get("/hello-world", (_, res) => {
     res.render("hello-world 😎");
 });
 
